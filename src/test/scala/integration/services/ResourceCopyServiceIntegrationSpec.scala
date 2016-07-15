d0fdfbc3c3e3eb08c77f169e7697641e5114e03e
Arpan Chaudhury <arpanch@thoughtworks.com>
2016-07-16 03:27:11 +0530

    added file copy service integration test for file transfer via FTP

--- src/test/scala/integration/services/ResourceCopyServiceIntegrationSpec.scala
+++ src/test/scala/integration/services/ResourceCopyServiceIntegrationSpec.scala
@@ -2,6 +2,8 @@ package integration.services
 
 import java.io.File
 
+import org.mockftpserver.fake.{FakeFtpServer, UserAccount}
+import org.mockftpserver.fake.filesystem.{DirectoryEntry, FileEntry, UnixFakeFileSystem}
 import org.specs2.mutable.Specification
 import org.specs2.specification.BeforeAfterAll
 import parsers.ResourceUrlParser
@@ -11,6 +13,9 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 class ResourceCopyServiceIntegrationSpec extends Specification with BeforeAfterAll {
+  val fakeFtpServer = new FakeFtpServer()
+  val fileSystem = new UnixFakeFileSystem()
+  val account = new UserAccount("user", "password", "/")
 
   "Resource Copy Service" >> {
     val resourceUrlParser = new ResourceUrlParser
@@ -18,14 +23,17 @@ class ResourceCopyServiceIntegrationSpec extends Specification with BeforeAfterA
 
     "should copy a list of resources into destination directory" >> {
       val resources = List(
+        "ftp://user:password@127.0.0.1:20000/data/some-ftp-file.txt",
         "http://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf",
         s"file://${new File("src/test/resources/local-file.txt").getAbsolutePath}"
       )
       val destinationDirectory = "target/tmp"
       val future = resourceCopyService.copyResources(resources, destinationDirectory)
 
-      Await.result(future, Duration.Inf) mustEqual List("done", "done")
+      Thread.sleep(500)
+      Await.result(future, Duration.Inf) mustEqual List("done", "done", "done")
       new File("target/tmp/pdf-sample.pdf").exists() mustEqual true
+      new File("target/tmp/some-ftp-file.txt").exists() mustEqual true
       new File("target/tmp/local-file.txt").exists() mustEqual true
     }
   }
@@ -34,6 +42,13 @@ class ResourceCopyServiceIntegrationSpec extends Specification with BeforeAfterA
     val tmpDirectory = new File("target/tmp")
     if(!tmpDirectory.exists()) tmpDirectory.mkdir()
     else tmpDirectory.listFiles().map(_.delete())
+
+    fakeFtpServer.setServerControlPort(20000)
+    fakeFtpServer.addUserAccount(account)
+    fileSystem.add(new DirectoryEntry("/data"))
+    fileSystem.add(new FileEntry("/data/some-ftp-file.txt", "useless file"))
+    fakeFtpServer.setFileSystem(fileSystem)
+    fakeFtpServer.start()
   }
 
   override def afterAll(): Unit = {
@@ -42,5 +57,6 @@ class ResourceCopyServiceIntegrationSpec extends Specification with BeforeAfterA
       tmpDirectory.listFiles().map(_.delete())
       tmpDirectory.delete()
     }
+    fakeFtpServer.stop()
   }
 }
